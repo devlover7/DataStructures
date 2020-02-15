a453a8df2fc4aec4c04bd056f2a26be6ea7b2bc2
abhinav <abhinavgr007@hotmail.com>
2020-02-15 20:52:19 +0530

    Custom ArrayList

--- src/prim/arraylist/ArrayList.java
+++ src/prim/arraylist/ArrayList.java
@@ -1,5 +1,59 @@
 package prim.arraylist;
 
 public class ArrayList {
+	static int ar[];
+	int increment=5;
+	static int nextposition=1;
+	ArrayList()
+	{
+		ar = new int[increment];
+	}
 
+	boolean add(int number)
+	{
+		boolean flag = false;
+
+		if(nextposition<ar.length)
+		{
+			ar [nextposition-1] = number;
+			nextposition ++;
+			flag = true;
+			return flag;
+		}
+		else 
+		{
+			int[] tempar = new int[ar.length+increment];
+			for(int i =0; i<ar.length;i++)
+			{
+				tempar[i]= ar[i];
+			}
+			ar = new int[tempar.length];
+			ar = tempar;
+			add(number);
+		}
+		return flag;
+	}
+
+
+	void print ()
+	{
+
+		for(int i=0;i<ar.length;i++)
+		{
+			System.out.print(ar[i]+" ");
+		}
+
+	}
+
+	int get(int index)
+	{
+		if(index<ar.length)
+		{
+			return ar[index];
+		}
+		else
+		{
+			return -1;
+		}
+	}
 }
--- src/prim/arraylist/DriverClass.java
+++ src/prim/arraylist/DriverClass.java
@@ -2,6 +2,15 @@ package prim.arraylist;
 
 public class DriverClass {
 public static void main(String[] args) {
+	ArrayList arr = new ArrayList();
+	
+	arr.add(22);
+	arr.add(23);
+	arr.add(24);
+	arr.add(24);
+	arr.print();
+	System.out.println(arr.get(30));
+	
 	
 }
 }
